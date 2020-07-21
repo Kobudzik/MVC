9a9a93461924cdc544e91b7895117d049b87e5b7
Kobudzik <kobudzik98@gmail.com>
2020-07-21 14:06:26 +0200

    Skopiowanie projektów na git

---
+++ Test2/Controllers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Test2.Models;
+using Test2.ViewModels;
+
+namespace Test2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+
+
+
+    }
+}
---
+++ Test2/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.UI;
+using Test2;
+using Test2.Models;
+using Test2.ViewModels;
+
+namespace Test2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+
+
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+
+
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year+ "/" +month);
+
+        }
+
+
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+
+    }
+}
