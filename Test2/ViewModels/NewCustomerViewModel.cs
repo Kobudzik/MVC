899b35b319a1f5a10483ec08cb337a6a2de17ac2
Kobudzik <kobudzik98@gmail.com>
2020-07-23 08:25:38 +0200

    Edit user form added

--- Test2/ViewModels/NewCustomerViewModel.cs
+++ Test2/ViewModels/NewCustomerViewModel.cs
@@ -7,7 +7,7 @@ using Test2.Models;
 
 namespace Test2.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
