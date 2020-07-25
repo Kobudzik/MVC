ecbfce4825a18c803336937aac781affc2f7fa95
Kobudzik <kobudzik98@gmail.com>
2020-07-25 15:23:14 +0200

    Added movie and customer API

---
+++ Test2/App_Start/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Test2.Dtos;
+using Test2.Models;
+
+namespace Test2.App_Start
+{
+    public class MappingProfile :Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
+
+
+            CreateMap<Movie, MovieDto>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
+
+            CreateMap<Customer, CustomerDto>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+        }
+    }
+}
---
+++ Test2/Dtos/MovieDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Test2.Models;
+
+namespace Test2.Dtos
+{
+    public class MovieDto
+    {
+        public byte Id { get; set; }
+        public byte GenreId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+
+        public DateTime DateAdded { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
